be0048a9398b375261628c02c0be57e0731b2a13
wecking <wecking1@gmail.com>
2016-10-23 21:05:45 +0100

    added automated test

---
+++ src/main/java/com.edeh.chigozie.elasticsearch.client.helper/Songs.java
@@ -0,0 +1,16 @@
+package com.edeh.chigozie.elasticsearch.client.helper;
+
+/**
+ * Created by kingsley on 10/23/16.
+ */
+public class Songs {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ src/test/java/com/edeh/chigozie/elasticsearch/client/helper/ElasticSearchCRUDTest.java
@@ -0,0 +1,37 @@
+package com.edeh.chigozie.elasticsearch.client.helper;
+
+import junit.framework.TestCase;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class ElasticSearchCRUDTest extends TestCase {
+
+    public void testCreateRecordWithObject() throws Exception {
+        Songs songs = new Songs();
+        songs.setTitle("love of my heart");
+        RestClient restClient = ElasticSearchClient.getHTTPClient("localhost", 9200);
+        Response response = ElasticSearchCRUD.createRecordWithObject(songs, restClient, "music", "songs");
+        assertThat(response.getStatusLine().getStatusCode(), equalTo(201));
+    }
+
+    public void testCreateRecordWithObject1() throws Exception {
+        Songs songs = new Songs();
+        songs.setTitle(" true love");
+        RestClient restClient = ElasticSearchClient.getHTTPClient("localhost", 9200);
+        Response response = ElasticSearchCRUD.createRecordWithObject(songs, restClient, "music", "songs");
+        assertThat(response.getStatusLine().getStatusCode(), equalTo(201));
+    }
+
+
+
+    public void testSearch() throws Exception {
+        Songs songs = new Songs();
+        songs.setTitle(" true love");
+        RestClient restClient = ElasticSearchClient.getHTTPClient("localhost", 9200);
+        Response response = ElasticSearchCRUD.search(restClient, "music", "songs", "love of my heart");
+        assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+    }
+}
